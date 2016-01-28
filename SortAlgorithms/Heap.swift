b8d59aeb6be30067b1d3ad915b6b4292271e8485
weizhou <shiwoow@gmail.com>
2016-01-28 14:41:29 +0800

    extract Heap

---
+++ SortAlgorithms/Heap.swift
@@ -0,0 +1,97 @@
+//
+//  Heap.swift
+//  SortAlgorithms
+//
+//  Created by 纬洲 冯 on 1/28/16.
+//  Copyright © 2016 dopcn. All rights reserved.
+//
+
+import Foundation
+
+protocol HeapType {
+    typealias T
+    var items: [T] { get set }
+    var heapSize: Int { get set }
+    func parent(i: Int) -> Int
+    func left(i: Int) -> Int
+    func right(i: Int) -> Int
+}
+
+extension HeapType {
+    func parent(i: Int) -> Int {
+        return i/2
+    }
+    
+    func left(i: Int) -> Int {
+        return i*2
+    }
+    
+    func right(i: Int) -> Int {
+        return i*2 + 1
+    }
+}
+
+struct MaxHeap<T: Comparable>: HeapType {
+    var items: [T]
+    var heapSize: Int
+    
+    mutating func maxHeapify(i: Int) {
+        let l = left(i), r = right(i)
+        var largest = -1
+        if l < heapSize && items[l] > items[i] {
+            largest = l
+        } else {
+            largest = i
+        }
+        if r < heapSize && items[r] > items[largest] {
+            largest = r
+        }
+        if largest != i {
+            (items[largest], items[i]) = (items[i], items[largest])
+            maxHeapify(largest)
+        }
+    }
+    
+    init(items: [T]) {
+        self.items = items
+        heapSize = items.count
+        var i = items.count/2
+        while i >= 0 {
+            maxHeapify(i)
+            i-=1
+        }
+    }
+}
+
+struct MinHeap<T: Comparable>: HeapType {
+    var items: [T]
+    var heapSize: Int
+    
+    mutating func minHeapify(i: Int) {
+        let l = left(i), r = right(i)
+        var smallest = -1
+        if l < heapSize && items[l] < items[i] {
+            smallest = l
+        } else {
+            smallest = i
+        }
+        if r < heapSize && items[r] < items[smallest] {
+            smallest = r
+        }
+        if smallest != i {
+            (items[smallest], items[i]) = (items[i], items[smallest])
+            minHeapify(smallest)
+        }       
+    }
+    
+    init(items: [T]) {
+        self.items = items
+        heapSize = items.count
+        var i = items.count/2
+        while i >= 0 {
+            minHeapify(i)
+            i-=1
+        }
+    }
+}
+
